f06f2f96f1779ed3122591e636c862a6bbfc010a
Han <quaint3@gmail.com>
2020-04-06 17:59:03 +0900

    [chore] 기본 프로젝트 생성

---
+++ BE/src/main/java/com/codesquad/todo8/Todo8Application.java
@@ -0,0 +1,13 @@
+package com.codesquad.todo8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todo8Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Todo8Application.class, args);
+  }
+
+}
