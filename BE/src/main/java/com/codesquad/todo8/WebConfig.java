9730e810d612e8e432ae6c5cfcfffce594e8add1
Han <quaint3@gmail.com>
2020-04-07 21:09:01 +0900

    [feat] webConfig 추가
    interceptor 클래스를 추가함.

---
+++ BE/src/main/java/com/codesquad/todo8/WebConfig.java
@@ -0,0 +1,16 @@
+package com.codesquad.todo8;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new JwtInterceptor())
+        .addPathPatterns("/**");
+
+  }
+}
