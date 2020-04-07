ef9662a0b437d2a206550cd71ab976acd01a64fb
Han <quaint3@gmail.com>
2020-04-07 22:12:48 +0900

    Test : 간단한 JWT 토근 비교 테스트 작성

--- BE/src/test/java/com/codesquad/todo8/JWTTest.java
+++ BE/src/test/java/com/codesquad/todo8/JWTTest.java
@@ -3,8 +3,11 @@ package com.codesquad.todo8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import java.security.Key;
 import javax.crypto.SecretKey;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -16,26 +19,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class JWTTest {
 
   private Logger logger = LoggerFactory.getLogger(JWTTest.class);
-  private static final String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiY2xpZW50In0.HEaCFpogl46sWOwmY2B53Sd09v89adcKW7_9gI-990c";
 
-  @Value("${jwt.token.clientSecret}")
+  @Value("${jwt.token.secret}")
   private String key;
 
   @Test
   public void 기본() {
 
-//    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-//    String secretString = Encoders.BASE64URL.encode(key.getEncoded());
-//    logger.info("secretString : {}", secretString);
+    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    String secretString = Encoders.BASE64.encode(key.getEncoded());
+    logger.info("secretString : {}", secretString);
 
-    SecretKey sk = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(key));
-
-    String client = "client";
-    String jws = Jwts.builder()
-        .claim("name", "client")
-        .signWith(sk)
-        .compact();
-    assertThat(jws).isEqualTo(jwt);
+//    SecretKey sk = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(key));
+//
+//    String client = "client";
+//    String jws = Jwts.builder()
+//        .claim("name", "client")
+//        .signWith(sk)
+//        .compact();
+//    assertThat(jws).isEqualTo(jwt);
   }
 
 }
