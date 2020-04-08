f3c5fb8af7341d6fc1c742e97442c2e2d1c349d0
koo bohyeon <goosee115@gmail.com>
2020-04-09 00:18:27 +0900

    Feature: user를 id값으로 조회하도록 수정함
    user를 id 값으로 찾는 것이 좋을 것 같아 수정함

--- BE/src/main/java/com/codesquad/todo8/repository/UserRepository.java
+++ BE/src/main/java/com/codesquad/todo8/repository/UserRepository.java
@@ -1,16 +1,16 @@
 package com.codesquad.todo8.repository;
 
 import com.codesquad.todo8.model.User;
-import java.util.Optional;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
-  @Query("SELECT * FROM user u WHERE u.id = :userId")
-  Optional<User> findById(@Param(value = "userId") String userId);
-
+    @Query("SELECT * FROM user u WHERE u.id = :id")
+    Optional<User> findById(@Param(value = "id") int id);
 }
