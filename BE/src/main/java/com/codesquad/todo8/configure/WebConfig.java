67b081cf1a450de6f0ac6bb931eb74358b39d6ae
Han <quaint3@gmail.com>
2020-04-08 17:01:45 +0900

    Fix : h2 console 제외

---
+++ BE/src/main/java/com/codesquad/todo8/configure/WebConfig.java
@@ -0,0 +1,23 @@
+package com.codesquad.todo8.configure;
+
+import com.codesquad.todo8.controller.JwtInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  private final JwtInterceptor jwtInterceptor;
+
+  public WebConfig(JwtInterceptor jwtInterceptor) {
+    this.jwtInterceptor = jwtInterceptor;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(jwtInterceptor)
+        .excludePathPatterns("/h2-console/**")
+        .addPathPatterns("/**");
+  }
+}
