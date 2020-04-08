e211ed3bbdf45d04389060992004479768664e3a
TTOzzi <devttozzi@gmail.com>
2020-04-08 21:58:27 +0900

    Feat: segue identifier 를 활용한 뷰 컨트롤러 재사용
    컨테이너 뷰를 갖고있는 뷰 컨트롤러에서 prepare 메소드를 사용해 segue 의 identifier 에 맞는 뷰 컨트롤러를 찾아 만들어둔 뷰 컨트롤러 인스턴스에 넣어줌

--- iOS/TodoApp/TodoApp/ViewController.swift
+++ iOS/TodoApp/TodoApp/ViewController.swift
@@ -9,6 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var todoViewController: TodoViewController?
+    private var progressingViewController: TodoViewController?
+    private var completeViewController: TodoViewController?
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "todo" {
+            todoViewController = segue.destination as? TodoViewController
+        } else if segue.identifier == "progressing" {
+            progressingViewController = segue.destination as? TodoViewController
+        } else if segue.identifier == "complete" {
+            completeViewController = segue.destination as? TodoViewController
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
