351477481f51c63d53bbc0de4a1618437b1ec87c
TTOzzi <devttozzi@gmail.com>
2020-04-08 20:21:40 +0900

    Feat: TodoCell 속성 업데이트 메소드 구현
    TodoCell 내부 Label 들을 IBOutlet 으로 바인딩
    필요한 정보를 받아와 업데이트 해주는 메소드 구현

--- iOS/TodoApp/TodoApp/View/TodoCell.swift
+++ iOS/TodoApp/TodoApp/View/TodoCell.swift
@@ -11,11 +11,21 @@ import UIKit
 class TodoCell: UITableViewCell {
     static let reuseIdentifier = "TodoCell"
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+    
+    func configure(title: String, content: String, author: String) {
+        self.titleLabel.text = title
+        self.contentLabel.text = content
+        self.authorLabel.text = "author by \(author)"
     }
 }
