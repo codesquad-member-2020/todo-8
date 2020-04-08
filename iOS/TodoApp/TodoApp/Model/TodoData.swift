dc6c4bb54cd8c10b73f4b65df2fe9fdae20a62b5
TTOzzi <devttozzi@gmail.com>
2020-04-08 17:36:47 +0900

    Feat: 디코드한 데이터를 담을 구조체 구현
    서버에서 받아오는 JSON 데이터 구조에 맞게 구현해줌

---
+++ iOS/TodoApp/TodoApp/Model/TodoData.swift
@@ -0,0 +1,28 @@
+//
+//  TodoData.swift
+//  TodoApp
+//
+//  Created by TTOzzi on 2020/04/08.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import Foundation
+
+struct TodoData: Codable {
+    var columns: [Column]
+}
+
+struct Column: Codable {
+    var id: String
+    var title: String
+    var cards: [Card]
+}
+
+struct Card: Codable {
+    var id: String
+    var title: String
+    var author: String
+    var contents: String
+    var createdDate: String
+    var modifiedDate: String
+}
