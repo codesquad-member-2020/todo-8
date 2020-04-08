e22e7f42ced49dd4fbc4a7936f9f906cde1e8971
TTOzzi <devttozzi@gmail.com>
2020-04-08 18:44:50 +0900

    Feat: DataManager 구현
    NetworkManager 로 받아온 데이터를 디코드 해 저장하는 객체

---
+++ iOS/TodoApp/TodoApp/Model/DataManager.swift
@@ -0,0 +1,28 @@
+//
+//  DataManager.swift
+//  TodoApp
+//
+//  Created by TTOzzi on 2020/04/08.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import Foundation
+
+class DataManager {
+    private var data: [Column]?
+    
+    func loadData(completion: @escaping () -> ()) {
+        let url = NetworkManager.serverUrl
+        NetworkManager.getRequest(url: url) { (data, _, error) in
+            guard let data = data else { return }
+            do {
+                let decoder = JSONDecoder()
+                let decodedData = try decoder.decode(TodoData.self, from: data)
+                self.data = decodedData.columns
+                completion()
+            } catch {
+                
+            }
+        }
+    }
+}
