6ba9ec13302fe1e89a9256ca2b5fff91796e3854
Han <quaint3@gmail.com>
2020-04-09 23:14:57 +0900

    Style : UserRepository 컨벤션 수정

--- BE/src/main/java/com/codesquad/todo8/repository/UserRepository.java
+++ BE/src/main/java/com/codesquad/todo8/repository/UserRepository.java
@@ -1,16 +1,16 @@
 package com.codesquad.todo8.repository;
 
 import com.codesquad.todo8.model.User;
+import java.util.Optional;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    @Query("SELECT * FROM user u WHERE u.id = :id")
-    Optional<User> findById(@Param(value = "id") int id);
+  @Query("SELECT u.id, u.user_name FROM user u WHERE u.user_name = :name")
+  Optional<User> findByName(@Param(value = "name") String userName);
+
 }
