91599ed3dc8326d0fcfc53aa349879f1092cf274
Han <quaint3@gmail.com>
2020-04-10 13:38:51 +0900

    Style : 주석 삭제

--- BE/src/main/java/com/codesquad/todo8/controller/TodoRestController.java
+++ BE/src/main/java/com/codesquad/todo8/controller/TodoRestController.java
@@ -23,33 +23,4 @@ public class TodoRestController {
 //    Long userId = Long.parseLong(request.getAttribute("userId").toString());
     return todoService.findAllContents(1L);
   }
-
-//    @PostMapping("/columns")
-//    public String addColumn(@RequestBody Column column) {
-//    Todo Column이 안 받아짐
-//        System.out.println("--------------------");
-//        User user = userRepository.findById(1).get();
-//        Column newColumn = new Column(column.getTitle());
-//        user.addColumn(newColumn);
-//        userRepository.save(user);
-//        return "ok";
-//    }
-//
-//    @PostMapping("/columns")
-//    public String addColumn() throws Exception {
-//        System.out.println("--------------------");
-//        User user = userRepository.findById(1).orElseThrow(Exception::new);
-//        Column column = new Column("testColumn");
-//        Column newColumn = new Column(column.getTitle());
-//        user.addColumn(newColumn);
-//        userRepository.save(user);
-//        return "ok";
-//    }
-//
-//    @PostMapping("/columns/{columnId}/cards")
-//    public void addCard(@PathVariable int columnId, @RequestBody Card card) throws Exception {
-//        User user = userRepository.findById(1).orElseThrow(Exception::new);
-//        user.getColumns().get(columnId).addCard(card);
-//        userRepository.save(user);
-//    }
 }
