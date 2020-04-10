faeef6211fe791fdbd037fbb2e81a3cb8156852f
Han <quaint3@gmail.com>
2020-04-10 12:22:34 +0900

    Refactor : main url 수정

--- BE/src/main/java/com/codesquad/todo8/controller/TodoRestController.java
+++ BE/src/main/java/com/codesquad/todo8/controller/TodoRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/category")
+@RequestMapping("/board")
 public class TodoRestController {
 
   private final TodoService todoService;
