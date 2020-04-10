41f765ebe72aff587c50061dd23a4dc1846001f7
Hu2y <sanghun_@daum.net>
2020-04-10 11:46:16 +0900

    Chore : main.js 불필요한 파일 제거 #40

--- FE/src/main.js
+++ FE/src/main.js
@@ -5,13 +5,3 @@ const controller = new Controller()
 document.addEventListener("DOMContentLoaded", () => {
   controller  
 })
-
-
-// $( init );
-
-// function init() {
-//   $( ".droppable-area1, .droppable-area2, .droppable-area3" ).sortable({
-//       connectWith: ".todo-list",
-//       stack: '.todo-list ul'
-//     }).disableSelection();
-// }
