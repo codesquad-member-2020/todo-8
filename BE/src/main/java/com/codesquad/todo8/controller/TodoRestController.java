e12dab42796a7c470efc9f6df7d7c8fe4372eda3
Han <quaint3@gmail.com>
2020-04-10 14:32:14 +0900

    Feat : mapping url 변경, 카드 추가 기능 추가

--- BE/src/main/java/com/codesquad/todo8/controller/TodoRestController.java
+++ BE/src/main/java/com/codesquad/todo8/controller/TodoRestController.java
@@ -1,26 +1,58 @@
 package com.codesquad.todo8.controller;
 
+import com.codesquad.todo8.model.Card;
+import com.codesquad.todo8.model.CardRequest;
 import com.codesquad.todo8.model.Category;
+import com.codesquad.todo8.model.User;
 import com.codesquad.todo8.service.TodoService;
+import com.codesquad.todo8.service.UserService;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/board")
 public class TodoRestController {
 
+  private static Logger logger = LoggerFactory.getLogger(TodoRestController.class);
   private final TodoService todoService;
+  private final UserService userService;
 
-  public TodoRestController(TodoService todoService) {
+  public TodoRestController(TodoService todoService,
+      UserService userService) {
     this.todoService = todoService;
+    this.userService = userService;
   }
 
-  @GetMapping
+  @GetMapping("board")
   public List<Category> main(HttpServletRequest request) {
-//    Long userId = Long.parseLong(request.getAttribute("userId").toString());
-    return todoService.findAllContents(1L);
+    Long id = getUserId(request);
+    return todoService.findAllContents(id);
+  }
+
+  @PostMapping("/cards")
+  public Card createCard(@RequestBody CardRequest cardRequest) {
+    return todoService.createCard(Card.of(
+        cardRequest.getCategoryId(),
+        cardRequest.getAuthor(),
+        cardRequest.getTitle(),
+        cardRequest.getContents()
+    ));
+  }
+
+  private Long getUserId(HttpServletRequest request) {
+    String userName = request.getAttribute("userName").toString();
+    User user = null;
+    try {
+      user = userService.getUserByName(userName).orElseThrow(NotFoundException::new);
+    } catch (NotFoundException e) {
+      logger.debug("UserName Not Found : {}", userName);
+    }
+    return user.getId();
   }
 }
