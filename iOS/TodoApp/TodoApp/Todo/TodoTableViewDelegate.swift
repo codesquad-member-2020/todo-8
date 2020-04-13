e68aec500b690dfcf04bf134bcfc4643a082033d
TTOzzi <devttozzi@gmail.com>
2020-04-13 22:08:05 +0900

    Refactor: contextMenu 에서 UIAction 을 생성하는 부분 메소드로 분리

--- iOS/TodoApp/TodoApp/Todo/TodoTableViewDelegate.swift
+++ iOS/TodoApp/TodoApp/Todo/TodoTableViewDelegate.swift
@@ -30,22 +30,40 @@ class TodoTableViewDelegate: NSObject, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
         return UIContextMenuConfiguration(identifier: nil, previewProvider: nil, actionProvider: { _ in
-            let moveToDone = UIAction(title: "Move to done", image: UIImage(systemName: "paperplane")) { _ in
-                guard let card = self.dataSource.getCard(at: indexPath) else { return }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.67) {
-                    self.dataSource.removeCard(at: indexPath)
-                }
-                NotificationCenter.default.post(name: TodoTableViewDelegate.moveToDone, object: nil, userInfo: [TodoTableViewDelegate.moveToDone:card])
-            }
-            let edit = UIAction(title: "Edit", image: UIImage(systemName: "pencil.and.outline")) { _ in
-                self.presentingViewController.presentEditingCardView(with: self.dataSource.getCard(at: indexPath), selectedIndex: indexPath)
-            }
-            let delete = UIAction(title: "Delete", image: UIImage(systemName: "trash"), attributes: .destructive) { _ in
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.67) {
-                    self.dataSource.removeCard(at: indexPath)
-                }
-            }
+            let moveToDone = self.moveToDoneAction(indexPath: indexPath)
+            let edit = self.editAction(indexPath: indexPath)
+            let delete = self.deleteAction(indexPath: indexPath)
             return UIMenu(title: "", children: [moveToDone, edit, delete])
         })
+    }
+    
+    func moveToDoneAction(indexPath: IndexPath) -> UIAction {
+        let title = "Move to done"
+        let image = UIImage(systemName: "paperplane")
+        return UIAction(title: title, image: image) { _ in
+            guard let card = self.dataSource.getCard(at: indexPath) else { return }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.67) {
+                self.dataSource.removeCard(at: indexPath)
+            }
+            NotificationCenter.default.post(name: TodoTableViewDelegate.moveToDone, object: nil, userInfo: [TodoTableViewDelegate.moveToDone:card])
+        }
+    }
+    
+    func editAction(indexPath: IndexPath) -> UIAction {
+        let title = "Edit"
+        let image = UIImage(systemName: "pencil.and.outline")
+        return UIAction(title: title, image: image) { _ in
+            self.presentingViewController.presentEditingCardView(with: self.dataSource.getCard(at: indexPath), selectedIndex: indexPath)
+        }
+    }
+    
+    func deleteAction(indexPath: IndexPath) -> UIAction {
+        let title = "Delete"
+        let image = UIImage(systemName: "trash")
+        return UIAction(title: title, image: image, attributes: .destructive) { _ in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.67) {
+                self.dataSource.removeCard(at: indexPath)
+            }
+        }
     }
 }
