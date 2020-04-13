8e85ce230dce25255f73ba6e49e83f1c47785fc9
Han <quaint3@gmail.com>
2020-04-13 22:28:21 +0900

    Feat : Activity class에 각각 로그 타입에 맞는 static 생성자 추가, equals, hashcode 추가

--- BE/src/main/java/com/codesquad/todo8/model/Activity.java
+++ BE/src/main/java/com/codesquad/todo8/model/Activity.java
@@ -1,10 +1,13 @@
 package com.codesquad.todo8.model;
 
+import com.google.common.base.Objects;
 import java.time.LocalDateTime;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table
 public class Activity {
 
   @Id
@@ -23,21 +26,48 @@ public class Activity {
   @Column(value = "create_at")
   private LocalDateTime createdTime;
 
-  public Activity() {
+  private static LocalDateTime getNow() {
+    return LocalDateTime.now();
   }
 
-  public Activity(String author, String action, String targetName, Long departure, Long arrival) {
+  private Activity(Long id, String author, String action, String targetName, Long departure,
+      Long arrival) {
+    this.id = id;
     this.author = author;
     this.action = action;
     this.targetName = targetName;
     this.departure = departure;
     this.arrival = arrival;
+    this.createdTime = getNow();
   }
 
-  public Activity(String author, String action, String targetName) {
+  private Activity(Long id, String author, String action, String targetName) {
+    this.id = id;
     this.author = author;
     this.action = action;
     this.targetName = targetName;
+    this.createdTime = getNow();
+  }
+
+  public static Activity add(String author, String action, String targetName) {
+    return new Activity(null, author, action, targetName);
+  }
+
+  public static Activity remove(String author, String action, String targetName) {
+    return new Activity(null, author, action, targetName);
+  }
+
+  public static Activity update(String author, String action, String targetName, Long departure,
+      Long arrival) {
+    return new Activity(null, author, action, targetName, departure, arrival);
+  }
+
+  public static Activity move(String author, String action, String targetName, Long departure,
+      Long arrival) {
+    return new Activity(null, author, action, targetName, departure, arrival);
+  }
+
+  public Activity() {
   }
 
   public LocalDateTime getCreatedTime() {
@@ -66,6 +96,23 @@ public class Activity {
 
   public Long getArrival() {
     return arrival;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Activity activity = (Activity) o;
+    return Objects.equal(id, activity.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
   }
 
   @Override
