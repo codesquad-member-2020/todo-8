4dd71fca6b3e876d484e5e51b215a18d5b1b044f
Han <quaint3@gmail.com>
2020-04-13 11:27:40 +0900

    Fix : setter 추가

--- BE/src/main/java/com/codesquad/todo8/model/CardRequest.java
+++ BE/src/main/java/com/codesquad/todo8/model/CardRequest.java
@@ -14,15 +14,31 @@ public class CardRequest {
     return categoryId;
   }
 
+  public void setCategoryId(Long categoryId) {
+    this.categoryId = categoryId;
+  }
+
   public String getAuthor() {
     return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
   }
 
   public String getTitle() {
     return title;
   }
 
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
   public String getContents() {
     return contents;
+  }
+
+  public void setContents(String contents) {
+    this.contents = contents;
   }
 }
