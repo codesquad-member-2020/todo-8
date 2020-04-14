54d3300f5616b3fce67c2a9f459c311a0c421e2e
Han <quaint3@gmail.com>
2020-04-14 13:57:12 +0900

    Fix : cors add

--- BE/src/main/java/com/codesquad/todo8/configure/WebConfig.java
+++ BE/src/main/java/com/codesquad/todo8/configure/WebConfig.java
@@ -2,16 +2,27 @@ package com.codesquad.todo8.configure;
 
 import com.codesquad.todo8.controller.JwtInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
   private final JwtInterceptor jwtInterceptor;
 
   public WebConfig(JwtInterceptor jwtInterceptor) {
     this.jwtInterceptor = jwtInterceptor;
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("*");
+
   }
 
   @Override
