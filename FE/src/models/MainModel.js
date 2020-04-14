e0c394654c9a28e7a0f4bf18d0e9fd97e674e2b3
Hu2y <sanghun_@daum.net>
2020-04-14 20:26:03 +0900

    Feat : 초기랜더링을 위한 main Model 정의 #19

--- FE/src/models/MainModel.js
+++ FE/src/models/MainModel.js
@@ -2,17 +2,11 @@ import { todoApi } from '../api/TodoRequest.js'
 
 class MainModel {
   constructor() {
-    this.initialize()
   }
 
-  initialize() {
-    this.fetchInitRenderData()
-  }
-
-  fetchInitRenderData() {
-    const URL = 'http://34.236.252.205/api/board'
-    todoApi.initRenderRequest(URL)
-      .then(data => console.log(data))
+  async fetchInitRenderData(URL) {
+    this.todoData = await todoApi.initRenderRequest(URL)
+    return this.todoData
   }
 }
 
