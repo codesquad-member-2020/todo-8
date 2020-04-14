367bf7e84bafaff71ad01c9ebd92d21c04695c1d
Hu2y <sanghun_@daum.net>
2020-04-14 20:27:48 +0900

    Feat : 컨트롤러에 view, model 주입 #19

--- FE/src/main.js
+++ FE/src/main.js
@@ -1,9 +1,26 @@
 import Controller from './controller/controller.js'
 import MainModel from './models/MainModel.js'
+import TodoView from './views/TodoView.js'
 
+import HeaderComponent from './components/HeaderComponent.js'
+import MenuComponent from './components/MenuComponent.js'
+import ColumnComponent from './components/ColumnComponent.js'
+
+
+const headerComponent = new HeaderComponent()
+const menuComponent = new MenuComponent()
+const columnComponent = new ColumnComponent()
+
+const todoView = new TodoView({
+  headerComponent,
+  menuComponent,
+  columnComponent
+})
 const mainModel = new MainModel()
+
 const controller = new Controller({
-  mainModel
+  mainModel,
+  todoView
 })
 
 document.addEventListener("DOMContentLoaded", () => {
