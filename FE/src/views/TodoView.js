5793fc817ccedd01ba18214fad8440e4e2388996
Hu2y <sanghun_@daum.net>
2020-04-14 20:27:03 +0900

    Feat : todoView 초기 구성 #19
    - 컨트롤러에서 모델에서 요청한 데이터를 view가 잘받아주는지 확인

---
+++ FE/src/views/TodoView.js
@@ -0,0 +1,11 @@
+class TodoView {
+  constructor({headerComponent}) {
+    this.headerComponent = headerComponent
+  }
+
+  render(todoData) {
+    console.log(todoData)
+  }
+}
+
+export default TodoView
