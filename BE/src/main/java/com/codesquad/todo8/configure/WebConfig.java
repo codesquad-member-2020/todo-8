fa8f0868fd1680bed8999b94775d55c54d745302
Han <quaint3@gmail.com>
2020-04-14 16:48:08 +0900

    Fix : LocalDateTime 배열로 나오던 문제 해결

--- BE/src/main/java/com/codesquad/todo8/configure/WebConfig.java
+++ BE/src/main/java/com/codesquad/todo8/configure/WebConfig.java
@@ -1,28 +1,22 @@
 package com.codesquad.todo8.configure;
 
 import com.codesquad.todo8.controller.JwtInterceptor;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
   private final JwtInterceptor jwtInterceptor;
 
   public WebConfig(JwtInterceptor jwtInterceptor) {
     this.jwtInterceptor = jwtInterceptor;
-  }
-
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("*")
-        .allowedMethods("*");
-
   }
 
   @Override
