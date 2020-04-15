9fd93b7cace2c99690d44e9b3294b00a9b384281
Hu2y <sanghun_@daum.net>
2020-04-15 09:42:12 +0900

    Feat :  menu, header 컴포넌트 랜더링 완료 #19

--- FE/src/views/TodoView.js
+++ FE/src/views/TodoView.js
@@ -1,10 +1,23 @@
 class TodoView {
-  constructor({headerComponent}) {
-    this.headerComponent = headerComponent
+  constructor({
+    headerComponent,
+    menuComponent,
+    columnComponent,
+    cardComponent
+  }) {
+    this.headerComponent = headerComponent;
+    this.menuComponent = menuComponent;
+    this.columnComponent = columnComponent;
+    this.cardComponent = cardComponent
+    this.todoApp = document.querySelector('.wrapper')
   }
 
   render(todoData) {
-    console.log(todoData)
+    this.todoApp.insertAdjacentHTML('afterbegin', this.headerComponent.render())
+    this.todoApp.insertAdjacentHTML('beforeend', this.menuComponent.render(todoData.activity))
+    this.todoApp.insertAdjacentHTML('beforeend', this.columnComponent.render(todoData.category))
+
+    // console.log(todoData)
   }
 }
 
