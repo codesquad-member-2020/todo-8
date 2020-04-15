632607e89b8fba70058e40ec1f50c8b9d910c828
Hu2y <sanghun_@daum.net>
2020-04-15 09:40:39 +0900

    Feat : menu컴포넌트 초기렌더링 구현

--- FE/src/components/MenuComponent.js
+++ FE/src/components/MenuComponent.js
@@ -1,14 +1,46 @@
 class MenuComponent { 
   constructor() {
+    this.computedLogTime()
+  }
 
+  computedLogTime() {
+    let date = new Date()
+    console.log(date);
   }
 
   render(logData) {
     const logList = logData.reduce((list, log) => {
       list += `
-      
+      <div class="ui feed" data-id=${log.id}>
+            <div class="event">
+              <div class="label">
+                <img src="./assets/image/cat.jpeg" />
+              </div>
+              <div class="content">
+                <div class="date">
+                  <font style="vertical-align: inherit;">
+                    ${log.createdTime}
+                  </font>
+                </div>
+                <div class="summary">
+                  <a>
+                    <font style="vertical-align: inherit;">@${log.author}</font>
+                  </a>
+                    <font style="vertical-align: inherit;">${log.action}</font>
+                  <a>
+                    <font style="vertical-align: inherit;">
+                      ${log.targetName}
+                    </font>
+                  </a>
+                  <font style="vertical-align: inherit;">
+                    to 하는중
+                  </font>
+                </div>
+              </div>
+            </div>
+          </div>
       `
-      return logList
+      return list
     } ,"") 
     return `
       <div class="menu-container">
@@ -20,6 +52,10 @@ class MenuComponent {
           <i class="bell icon"></i>
           <span>Activity</span>
         </div>
+        <div class="feed-container">${logList}</div>
+        <button class="menu-close-btn">
+          <i class="x icon"></i>
+        </button>
       </div>
     `
   }
