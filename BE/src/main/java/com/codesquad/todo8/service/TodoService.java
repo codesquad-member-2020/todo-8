96194c32c03fe1e803ddb6e1999caf2af6155a23
Han <quaint3@gmail.com>
2020-04-16 20:23:54 +0900

    Fix : add, updated, deleted시 arrival categoryid가 기록되도록 수정

--- BE/src/main/java/com/codesquad/todo8/service/TodoService.java
+++ BE/src/main/java/com/codesquad/todo8/service/TodoService.java
@@ -123,6 +123,7 @@ public class TodoService {
         .author(card.getAuthor())
         .action(action)
         .targetName(card.getTitle())
+        .arrival(card.getCategoryId())
         .createdTime(now())
         .build();
   }
