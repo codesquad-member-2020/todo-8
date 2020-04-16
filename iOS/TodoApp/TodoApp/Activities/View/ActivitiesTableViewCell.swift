438a3ac5709bf4b301d3bc8531b1c61c06f79361
TTOzzi <devttozzi@gmail.com>
2020-04-16 18:15:42 +0900

    Feat: 프로토타입 셀 UI 구현
    프로토타입 셀 UI 를 스토리보드에서 구현하고 ActivitiesTableViewCell 커스텀 클래스를 만들어 ImageView 와 Label 연결

---
+++ iOS/TodoApp/TodoApp/Activities/View/ActivitiesTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ActivitiesTableViewCell.swift
+//  TodoApp
+//
+//  Created by TTOzzi on 2020/04/16.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class ActivitiesTableViewCell: UITableViewCell {
+    static let reuseIdentifier = "ActivitiesCell"
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var activities: UILabel!
+    @IBOutlet weak var timeRecord: UILabel!
+}
