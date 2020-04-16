bd3ee999e9923a11f63a59c56c6af89d9a9bf58d
TTOzzi <devttozzi@gmail.com>
2020-04-16 18:04:47 +0900

    Feat: ActivitiesViewController 커스텀클래스 생성
    Activites 화면 뷰 컨트롤러의 커스텀 클래스로 설정해주고 tableView 바인딩

---
+++ iOS/TodoApp/TodoApp/Activities/ActivitiesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ActivitiesViewController.swift
+//  TodoApp
+//
+//  Created by TTOzzi on 2020/04/16.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class ActivitiesViewController: UIViewController {
+    @IBOutlet weak var activitiesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
