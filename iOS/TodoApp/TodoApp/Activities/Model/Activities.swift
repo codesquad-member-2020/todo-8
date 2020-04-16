d1ea6f30db80b7f45f1313ba2649d1939f9a16e4
TTOzzi <devttozzi@gmail.com>
2020-04-16 19:11:09 +0900

    Feat: activities 데이터를 담을 구조체 구현

---
+++ iOS/TodoApp/TodoApp/Activities/Model/Activities.swift
@@ -0,0 +1,22 @@
+//
+//  Activities.swift
+//  TodoApp
+//
+//  Created by TTOzzi on 2020/04/16.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import Foundation
+
+struct ActivitiesResponse: Codable {
+    var response: Activities
+}
+
+struct Activities: Codable {
+    var targetName: String
+    var author: String
+    var action: String
+    var departure: Int?
+    var arrival: Int?
+    var createdTime: String
+}
